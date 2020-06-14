e4c686d324eae10d17dbb20821313d4fde4d3d40
Alexander Konshin <sashulya.konshin@gmail.com>
2020-06-14 23:22:31 +0300

    Add train/val splitting.

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEST_IMAGES_DIR = os.path.join(BASE_DIR, 'data/test')
+TRAIN_IMAGES_DIR = os.path.join(BASE_DIR, 'data/train')
+TRAIN_LABELS_FILE = os.path.join(BASE_DIR, 'data/train.csv')
+SAMPLE_SUBMISSION_FILE = os.path.join(BASE_DIR, 'data/sample_submission.csv')
+
+TRAIN_SIZE = 0.9
+SEED = 42
---
+++ src/train.py
@@ -0,0 +1,61 @@
+import json
+import os
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import GroupShuffleSplit
+
+import config
+
+
+def read_train_csv(train_csv_path: str, train_images_dir: str):
+    dataframe = pd.read_csv(train_csv_path)
+    image_ids = pd.unique(dataframe['image_id']).tolist()
+    image_sources = []
+    image_bboxes = []
+    image_paths = []
+    for image_id in image_ids:
+        id_items = dataframe[dataframe['image_id'] == image_id]
+        image_sources.append(pd.unique(id_items['source']).tolist()[0])
+        image_bbox_strings = id_items['bbox'].tolist()
+        image_bbox_numbers = []
+        for bbox_string in image_bbox_strings:
+            image_bbox_numbers.append(json.loads(bbox_string))
+        image_bboxes.append(image_bbox_numbers)
+        image_paths.append(os.path.join(config.TRAIN_IMAGES_DIR, f'{image_id}.jpg'))
+
+    return image_paths, image_bboxes, image_sources
+
+
+def get_train_val_split(image_paths, image_bboxes, image_sources, seed, train_size):
+    gss = GroupShuffleSplit(n_splits=1, train_size=train_size, random_state=seed)
+    train_idx, val_idx = next(gss.split(image_paths, image_bboxes, image_sources))
+    train_image_paths = np.array(image_paths)[train_idx].tolist()
+    train_image_bboxes = np.array(image_bboxes)[train_idx].tolist()
+    val_image_paths = np.array(image_paths)[val_idx].tolist()
+    val_image_bboxes = np.array(image_bboxes)[val_idx].tolist()
+    return train_image_paths, train_image_bboxes, val_image_paths, val_image_bboxes
+
+
+if __name__ == '__main__':
+    tf.random.set_seed(22)
+    np.random.seed(22)
+
+    image_paths, image_bboxes, image_sources = read_train_csv(
+        train_csv_path=config.TRAIN_LABELS_FILE,
+        train_images_dir=config.TRAIN_IMAGES_DIR
+    )
+
+    train_image_paths, train_image_bboxes, val_image_paths, val_image_bboxes = \
+        get_train_val_split(
+            image_paths=image_paths,
+            image_bboxes=image_bboxes,
+            image_sources=image_sources,
+            seed=config.SEED,
+            train_size=config.TRAIN_SIZE
+        )
+
+    print(len(image_paths))
+    print(len(train_image_paths))
+    print(len(val_image_paths))
